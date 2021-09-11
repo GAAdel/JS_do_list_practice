a2b5a914c3c6bea19b2a1371edfef67dc1bbab52
GAAdel <32236445+GAAdel@users.noreply.github.com>
2021-09-11 20:06:49 +0300

    created array, added  event onclick

---
+++ script/script.js
@@ -0,0 +1,20 @@
+let message_input = document.querySelector('.widget_input');
+let message_button = document.querySelector('.widget_btn');
+let tasks_todo = document.querySelector('.widget__task');
+
+let mas = [];
+
+message_button.onclick = function(){
+  
+  let new_task = {
+    task: message_input.value,
+    checked: false,
+    important: false
+  };
+
+  mas.push(new_task);
+  console.log(mas);
+  message_input.value = "";
+
+};
+   
